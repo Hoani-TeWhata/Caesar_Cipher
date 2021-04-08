2ac4c0d31fdb2e4348d56315dae6e1a099295cb4
Hoani Te <>
2021-04-08 23:43:23 +0000

    This now shows me the location of the character in the alphabet but also shows me the new location

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ string_output = ""
 for i in range (n):
   character = string_input[i]
   location = alphabets.find(character)
-  print(character, location)
   new_location = (location + key) % 26
+  print(character, location, new_location)
   string_output += alphabets[new_location]
 
 print(string_output)
