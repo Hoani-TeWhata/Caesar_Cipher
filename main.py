ee9b7a8325b675173c27e057a120bf1279b9da5b
Hoani Te <>
2021-04-09 00:00:12 +0000

    I have added a loop which goes to 3 and then stops the code

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-alphabets = 'abcdefghijklmnoqprstuvwxyz'
+count = 0
+while count < 3:
+ 
+ alphabets = 'abcdefghijklmnoqprstuvwxyz'
 
-string_input = input("Enter your message: ")
-key = int(input("Enter your key: "))
+ string_input = input("Enter your message: ")
+ key = int(input("Enter your key: "))
 
-n = len(string_input)
+ n = len(string_input)
 
-string_output = ""
+ string_output = ""
 
-for i in range (n):
-  character = string_input[i]
-  location = alphabets.find(character)
-  new_location = (location + key) % 26
-  print(character, location, new_location)
-  string_output += alphabets[new_location]
+ for i in range (n):
+   character = string_input[i]
+   location = alphabets.find(character)
+   new_location = (location + key) % 26
+   print(character, location, new_location)
+   string_output += alphabets[new_location]
 
-print(string_output)
+ print(string_output)
+ 
+ 
+ count = count + 1 
+
